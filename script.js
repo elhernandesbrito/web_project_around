93afabf35649468afa9f41afbb58457578ea5602
ElisHernandes <elhernandes@yahoo.com.br>
2023-09-20 17:47:40 -0300

    project 4

---
+++ script.js
@@ -0,0 +1,51 @@
+const editarPerfil = document.querySelector('.profile__edit');
+const profile = document.querySelector('.profile');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__closeButton');
+const submitSave = document.querySelector('.popup__submit-save');
+const popupForm = document.querySelector('.popup__form');
+const inputName = popupForm.querySelector('.popup__form-name');
+const inputExplorar = popupForm.querySelector('.popup__form-Explorar');
+const profileName = document.querySelector('.profile__name');
+const profileExplorer = document.querySelector('.profile__explorer');
+const header = document.querySelector('.header');
+const elements = document.querySelector('.elements');
+const copyright = document.querySelector('.copyright');
+const cards = document.querySelector('.cards');
+let card = cards.querySelector('.cards__card');
+let like = document.querySelectorAll('.cards__card-like');
+
+
+editarPerfil.addEventListener('click', toggleFormDisplay)
+closeButton.addEventListener('click', changeDisplayToNone )
+submitSave.addEventListener('click', handleProfileFormSubmit)
+
+
+function toggleFormDisplay() {
+   popup.classList.toggle('popup_change_display')
+}
+
+function changeDisplayToNone() {
+   popup.classList.remove('popup_change_display')
+}
+
+function handleProfileFormSubmit(event) {
+   event.preventDefault();
+   profileName.textContent =  inputName.value;
+   profileExplorer.textContent = inputExplorar.value;
+   inputName.value = '';
+   inputExplorar.value = '';
+   changeDisplayToNone();
+}
+
+
+
+
+
+
+
+
+
+
+
+
